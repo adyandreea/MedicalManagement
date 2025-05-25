604d9c08ed926942c1a7c02a5990493c80f01682
adyandreea <adyandreea48@gmail.com>
2025-05-25 21:06:34 +0300

    Add files via upload

---
+++ Tema2/php/specializari.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (!isset($_SESSION['logged_in'])) die("Acces interzis");
+require 'db.php';
+
+$actiune = $_POST['actiune'] ?? '';
+$id = $_POST['id'] ?? null;
+$nume = trim($_POST['nume'] ?? '');
+
+switch ($actiune) {
+    case 'adauga':
+        if ($nume !== '') {
+            $stmt = $pdo->prepare("INSERT INTO specializari (nume) VALUES (:nume)");
+            $stmt->execute(['nume' => $nume]);
+        }
+        break;
+
+    case 'modifica':
+        if ($id && $nume !== '') {
+            $stmt = $pdo->prepare("UPDATE specializari SET nume = :nume WHERE id = :id");
+            $stmt->execute(['nume' => $nume, 'id' => $id]);
+        }
+        break;
+
+    case 'sterge':
+        if ($id) {
+            $stmt = $pdo->prepare("DELETE FROM specializari WHERE id = :id");
+            $stmt->execute(['id' => $id]);
+        }
+        break;
+}
+
+header("Location: admin.php");
+exit;
